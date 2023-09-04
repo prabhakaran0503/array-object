4c71c1ce9e9e09f22b2ca8750fd02f43e5c2546d
prabhakaran <prabhakaranplkk@gmail.com>
2023-09-04 22:15:39 +0530

    array

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var libray=[
+    {
+        author:"bill gates",
+        title:"the road ahead",
+        readingStatus:true
+    },
+    {
+        author:"steve jobs",
+        title:"walter lsaacson",
+        readingStatus:true
+    },
+    {
+        author:"suzanne collins",
+        title:"mockingjay the final bank of the hunger games",
+        readingStatus:false
+    }
+]
